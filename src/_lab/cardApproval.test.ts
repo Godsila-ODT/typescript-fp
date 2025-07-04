f1b8f99556f33b5d4e93ee5a7bdcc1640427c677
Godsila <acgodsleeper@gmail.com>
2025-07-04 23:18:03 +0700

    CHORE: Immplement card approval

--- src/_lab/cardApproval.test.ts
+++ src/_lab/cardApproval.test.ts
@@ -1,31 +1,60 @@
+import { pipe } from "fp-ts/lib/function"
+import { filterApprove, filterReject, loadCardRequest, validateFileExists } from "./cardApproval"
+
 describe('Card Approval', () => {
   it('should return only approved request records', () => {
-    // expect(
-    //   pipe(
-    //     // file -> Either
-    //     // load card request from file -> Either
-    //     // filter approved -> Either
-    //   )
-    // ).toEqual(right - approved request)
+    const response = { "_tag": "Right", "right": [{ "name": "John", "salary": 18000, "hasEmpCert": true, "cardType": "silver" }, { "name": "Mary", "salary": 17500, "hasEmpCert": false, "cardType": "silver" }, { "name": "Peter", "salary": 35000, "hasEmpCert": true, "cardType": "gold" }, { "name": "Jane", "salary": 45000, "hasEmpCert": true, "cardType": "platinum" }, { "name": "Jim", "salary": 32000, "hasEmpCert": true, "cardType": "gold" }, { "name": "Jill", "salary": 22000, "hasEmpCert": false, "cardType": "silver" }, { "name": "Jack", "salary": 33000, "hasEmpCert": true, "cardType": "gold" }, { "name": "Jill", "salary": 40000, "hasEmpCert": false, "cardType": "silver" }, { "name": "Jim", "salary": 32000, "hasEmpCert": true, "cardType": "gold" }, { "name": "Sam", "salary": 60000, "hasEmpCert": true, "cardType": "diamond" }] }
+
+    expect(
+      pipe(
+        __dirname + "/data/cardRequest.csv",
+        validateFileExists,
+        loadCardRequest,
+        filterApprove,
+        // file -> Either
+        // load card request from file -> Either
+        // filter approved -> Either
+      )
+    ).toEqual(response)
   })
 
   it('should return only rejected request records', () => {
-    // expect(
-    //   pipe(
-    //     // file -> Either
-    //     // load card request from file -> Either
-    //     // filter rejected -> Either
-    //   )
-    // ).toEqual(right - rejected request)
+    const response = { "_tag": "Right", "right": [{ "cardType": undefined, "hasEmpCert": true, "name": "Jill", "salary": 9000 }] }
+    expect(
+      pipe(
+        __dirname + "/data/cardRequest.csv",
+        validateFileExists,
+        loadCardRequest,
+        filterReject,
+        // file -> Either
+        // load card request from file -> Either
+        // filter rejected -> Either
+      )
+    ).toEqual(response)
   })
 
   it('should return left error when file is corrupted', () => {
-    // expect(
-    //   pipe(
-    //     // file -> Either
-    //     // load card request from file and detect file corrupted -> Either
-    //     // filter approved (let error pass through) -> Either
-    //   )
-    // ).toEqual(left - file corrupted error)
+    const xx = pipe(
+      __dirname + "/data/errorRequest.csv",
+      validateFileExists,
+      loadCardRequest,
+      filterApprove,
+      // file -> Either
+      // load card request from file and detect file corrupted -> Either
+      // filter approved (let error pass through) -> Either
+    )
+
+    const failResponse = { "_tag": "Left", "left": "Error: Found unknown reject" }
+    expect(
+      pipe(
+        __dirname + "/data/errorRequest.csv",
+        validateFileExists,
+        loadCardRequest,
+        filterApprove,
+        // file -> Either
+        // load card request from file and detect file corrupted -> Either
+        // filter approved (let error pass through) -> Either
+      )
+    ).toEqual(failResponse)
   })
 })
--- src/_lab/cardApproval.ts
+++ src/_lab/cardApproval.ts
@@ -1,1 +1,122 @@
+import { existsSync, readFileSync } from 'fs'
+import { pipe } from "fp-ts/lib/function";
+import { Either, flatMap, left, right } from "fp-ts/lib/Either";
+import { filter } from "fp-ts/lib/Array";
+import path from 'path'
+import { ApprovalResult, cardCriteria, CardRequest } from './cardCriteria';
+
+type LoadCardRequest = (filePath: Either<string, string>) => Either<string, string[]>
+type FilterApprove = (data: Either<string, string[]>) => Either<string, ApprovalResult[]>
+type FilterReject = (data: Either<string, string[]>) => Either<string, CardRequest[]>
+
 // start here...
+
+export const validateFileExists = (filePath: string): Either<string, string> =>
+  !existsSync(path.resolve(filePath))
+    ? left(`Error: file not exist. ${filePath}`)
+    : right(filePath);
+
+
+export const loadCardRequest: LoadCardRequest =
+  flatMap(path => pipe(
+    readFileSync(path, 'utf-8'),
+    (content) => content.split(/\r?\n/),
+    filter(line => line.trim() !== ''),
+    (lines) => lines.length === 0
+      ? left('Error: File has not content')
+      : right(lines)
+  ))
+
+export const filterApprove: FilterApprove =
+  flatMap(timeStamps =>
+    pipe(
+      timeStamps,
+      mapCard,
+      approveResult,
+      filterApproved,
+    )
+  )
+
+const mapCard = (timeStampList: string[]): CardRequest[] =>
+  timeStampList.reduce(
+    (acc, line) => processLine(acc, line), [] as CardRequest[]
+  )
+
+const processLine = (acc: CardRequest[], line: string): CardRequest[] => {
+  if (line === "name,salary,has_emp_cer") return [];
+  const cardRequests = line.split(',')
+  if (cardRequests[0] !== '' && cardRequests[1] !== '' && cardRequests[2] !== '') {
+    return appendCardRequest(acc, cardRequests)
+  }
+  return []
+}
+
+const appendCardRequest = (cardRequest: CardRequest[], line: string[]): CardRequest[] =>
+  [...cardRequest, {
+    name: line[0],
+    salary: Number(line[1]),
+    hasEmpCert: line[2] === "true",
+  }]
+
+
+
+const approveResult = (cards: CardRequest[]): Either<string, ApprovalResult[]> =>
+  pipe(
+    cards,
+    mapCardInRange,
+    (cardApprove) => !cardApprove ?
+      left(`Error: Found unknown reject`) :
+      right(cardApprove)
+  )
+
+
+const mapCardInRange = (timeStampList: CardRequest[]): ApprovalResult[] =>
+  timeStampList.reduce(
+    (acc, line) => [...acc, cardInRange(line)], [] as ApprovalResult[]
+  )
+
+const cardInRange = (card: CardRequest): ApprovalResult => {
+  let cardType = cardCriteria.filter(cardCri => (cardCri.minSalary <= card.salary && cardCri.maxSalary >= card.salary) || card.salary >= cardCri.minSalary)
+  if (cardType.length > 0) {
+    cardType = cardType.filter(c => (c.requiredEmpCert && card.hasEmpCert) || !c.requiredEmpCert)
+  }
+  return {
+    ...card,
+    cardType: cardType[cardType.length - 1]?.cardType
+  }
+}
+
+const filterApproved = (cards: Either<string, ApprovalResult[]>): Either<string, ApprovalResult[]> =>
+  pipe
+    (cards._tag === 'Right' ? cards.right : [],
+      filterCard,
+      (cardApprove) => cardApprove.length <= 0 ?
+        left(`Error: Found unknown reject`) :
+        right(cardApprove)
+
+    )
+
+const filterCard = (c: ApprovalResult[]): ApprovalResult[] =>
+  c.filter(cc => cc.cardType)
+
+export const filterReject: FilterReject =
+  flatMap(timeStamps =>
+    pipe(
+      timeStamps,
+      mapCard,
+      approveResult,
+      filterRejected
+    )
+  )
+
+const filterRejected = (cards: Either<string, ApprovalResult[]>): Either<string, CardRequest[]> =>
+  pipe
+    (cards._tag === 'Right' ? cards.right : [],
+      filterRejectCard,
+      (cardApprove) => !cardApprove ?
+        left(`Error: Found unknown reject`) :
+        right(cardApprove)
+
+    )
+const filterRejectCard = (c: ApprovalResult[]): ApprovalResult[] =>
+  c.filter(cc => !cc.cardType)
